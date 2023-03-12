3010f60d58862847e0fdfdece4271fa7aa0ea547
JMSASanchezUwU <106613839+JMSASanchezUwU@users.noreply.github.com>
2023-03-11 20:28:15 -0600

    Add files via upload

---
+++ Display4/Display4.ino
@@ -0,0 +1,21 @@
+//Libreria
+#include <TM1637Display.h>
+
+//Pines
+#define CLK 4
+#define DIO 13
+#define SENSOR_PIN 19
+
+TM1637Display display(CLK, DIO);
+
+void setup() {
+  pinMode(SENSOR_PIN, INPUT_PULLUP);
+  display.setBrightness(0xff);
+}
+
+void loop() {
+  int sensorValue = digitalRead(SENSOR_PIN);
+  display.showNumberDec(sensorValue);
+  delay(500);
+  display.clear();
+}
